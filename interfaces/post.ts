b2181c25fc1676ec92435133bc709b5cc23ad311
aryan <aryan.jand@gmail.com>
2023-12-25 16:29:19 -0800

    prettier

--- interfaces/post.ts
+++ interfaces/post.ts
@@ -1,16 +1,14 @@
-import type Author from './author'
-
-type PostType = {
-  slug: string
-  title: string
-  date: string
-  coverImage: string
-  author: Author
-  excerpt: string
+type ContentType = {
+  author: any;
+  date: any;
+  slug: string;
+  title: string;
+  coverImage: string;
+  excerpt: string;
   ogImage: {
-    url: string
-  }
-  content: string
-}
+    url: string;
+  };
+  content: string;
+};
 
-export default PostType
+export default ContentType;
