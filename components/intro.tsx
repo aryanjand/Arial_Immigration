44b48246932e3a187a9c48d8bfafded798a0289c
aryan <aryan.jand@gmail.com>
2023-12-30 16:02:27 -0800

    Added header links to the intro header

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,26 @@
-const Intro = () => {
+import Link from "next/link";
+import HeaderLink from "../interfaces/headerlink";
+
+type Props = {
+  headerLinks: HeaderLink[];
+};
+
+const Intro = ({ headerLinks }: Props) => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Arial Immigration
       </h1>
+      {headerLinks.map((sn) => (
+        <Link
+          key={sn.slug}
+          href="/posts/[slug]"
+          as={`/posts/${sn.slug}`}
+          className="hover:underline"
+        >
+          {sn.title}
+        </Link>
+      ))}
     </section>
   );
 };
