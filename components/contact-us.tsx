e3b261dc1bdc0dc2de661d549901662900f2ba54
aryan <aryan.jand@gmail.com>
2023-12-31 16:59:55 -0800

    contact-us components used at bottom of many pages

---
+++ components/contact-us.tsx
@@ -0,0 +1,12 @@
+const ContactUs = () => {
+  return (
+    <section>
+      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+        Contact Us
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:grid-cols-3 lg:gap-x-32 gap-y-20 md:gap-y-32"></div>
+    </section>
+  );
+};
+
+export default ContactUs;
