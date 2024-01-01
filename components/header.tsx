0545b7bc25c4594a6e0bb732f19d43c4081ade8b
aryan <aryan.jand@gmail.com>
2023-12-31 17:00:11 -0800

    Added header links

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,36 @@
 import Link from "next/link";
+import cn from "classnames";
+import HeaderLink from "../interfaces/headerlink";
 
-const Header = () => {
+type Props = {
+  headerLinks: HeaderLink[];
+  slug: string;
+};
+
+const Header = ({ headerLinks, slug }: Props) => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Arial Immigration
-      </Link>
-    </h2>
+    <header className="mb-20 mt-8">
+      <nav className="flex flex-wrap items-center justify-between">
+        <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
+          <Link href="/" className="hover:underline">
+            Arial Immigration
+          </Link>
+        </h2>
+        {headerLinks.map((link) => (
+          <Link
+            key={link.slug}
+            href={`/header-link/${link.slug}`}
+            className={cn(
+              "hover:underline",
+              "mr-4",
+              { underline: link.slug === slug }, // Conditionally apply "underline" class
+            )}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </nav>
+    </header>
   );
 };
 
