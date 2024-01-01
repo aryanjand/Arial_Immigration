6b6914be32477df1d3d58e7ea155c496e11a8186
aryan <aryan.jand@gmail.com>
2023-12-31 17:21:08 -0800

    Build Errors Fixed and Prettier

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -8,23 +8,26 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <>
-    <div className="max-w-6xl mx-auto grid gap-5">
-      <h2 className="text-4xl font-light">{content.title}</h2>
-      <div className={`grid ${content.coverImage ? 'grid-cols-2' : 'grid-cols-1'} items-center gap-4`}>
-        {content.coverImage && (
-          <Image
-            src={content.coverImage}
-            alt={content.title}
-            width={300}
-            height={630}
-          />
-        )}
-        <p className="self-center">{content.excerpt}</p>
+      <div className="max-w-6xl mx-auto grid gap-5">
+        <h2 className="text-4xl font-light">{content.title}</h2>
+        <div
+          className={`grid ${
+            content.coverImage ? "grid-cols-2" : "grid-cols-1"
+          } items-center gap-4`}
+        >
+          {content.coverImage && (
+            <Image
+              src={content.coverImage}
+              alt={content.title}
+              width={300}
+              height={630}
+            />
+          )}
+          <p className="self-center">{content.excerpt}</p>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
-
 
 export default PostBody;
