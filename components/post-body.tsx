2dd2ce961ba55560ee5c2c293124528aae412157
aryan <aryan.jand@gmail.com>
2023-12-31 17:03:49 -0800

    Added Image and Title

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,18 +1,30 @@
-import markdownStyles from "./markdown-styles.module.css";
+import Image from "next/image";
+import ContentType from "../interfaces/post";
 
 type Props = {
-  content: string;
+  content: ContentType;
 };
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles["markdown"]}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+    <>
+    <div className="max-w-6xl mx-auto grid gap-5">
+      <h2 className="text-4xl font-light">{content.title}</h2>
+      <div className={`grid ${content.coverImage ? 'grid-cols-2' : 'grid-cols-1'} items-center gap-4`}>
+        {content.coverImage && (
+          <Image
+            src={content.coverImage}
+            alt={content.title}
+            width={300}
+            height={630}
+          />
+        )}
+        <p className="self-center">{content.excerpt}</p>
+      </div>
     </div>
+  </>
   );
 };
+
 
 export default PostBody;
