7a9f778ac9916bf8efc4639a0250ba264a7464e5
aryan <aryan.jand@gmail.com>
2023-12-31 23:17:06 -0800

    Optional types

--- interfaces/content-type.ts
+++ interfaces/content-type.ts
@@ -1,15 +1,15 @@
 type ContentType = {
-  author: any;
-  date: any;
-  slug: string;
-  title: string;
-  subHeading: string;
-  coverImage: string;
-  excerpt: string;
-  ogImage: {
-    url: string;
+  author?: any;
+  date?: any;
+  slug?: string;
+  title?: string;
+  subHeading?: string;
+  coverImage?: string;
+  excerpt?: string;
+  ogImage?: {
+    url?: string;
   };
-  content: ContentType[];
+  content?: ContentType[];
 };
 
 export default ContentType;
