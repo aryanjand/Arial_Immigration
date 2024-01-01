7cff55b5a2197bd5409d0e2f5f81f92df86f4f7f
aryan <aryan.jand@gmail.com>
2023-12-31 17:03:10 -0800

    Updated About us and uses server side rendering

--- pages/header-link/about-us.tsx
+++ pages/header-link/about-us.tsx
@@ -5,92 +5,95 @@ import PostBody from "../../components/post-body";
 import Header from "../../components/header";
 import PostHeader from "../../components/post-header";
 import Layout from "../../components/layout";
-import { getPostBySlug, getAllPosts } from "../../lib/api";
 import PostTitle from "../../components/post-title";
-import Head from "next/head";
-import { CMS_NAME } from "../../lib/constants";
-import markdownToHtml from "../../lib/markdownToHtml";
-import type PostType from "../../interfaces/post";
+import type ContentType from "../../interfaces/post";
+import CoverImage from "../../public/assets/images/hero/hero-about-us.png";
+import rcic_cric from "../../public/assets/images/hero/rcic-cric.png";
+import HeaderLink from "../../interfaces/headerlink";
 
 type Props = {
-  post: PostType;
-  morePosts: PostType[];
-  preview?: boolean;
+  post: ContentType;
+  headerLinks: HeaderLink[];
 };
 
-export default function Post({ post, morePosts, preview }: Props) {
+export default function Post({ post, headerLinks }: Props) {
   const router = useRouter();
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
+
   return (
-    <Layout preview={preview}>
+    <Layout /* preview={preview} - Remove or define `preview` */>
       <Container>
-        <Header />
+        <Header headerLinks={headerLinks} slug={post.slug} />
         {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>{title}</title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
+          <article className="mb-32">
+            <PostHeader title={post.title} coverImage={post.coverImage} />
+            {post.content.map((c) => (
+              <PostBody content={c} />
+            ))}
+          </article>
         )}
       </Container>
     </Layout>
   );
 }
-
-type Params = {
-  params: {
-    slug: string;
-  };
-};
-
-export async function getStaticProps({ params }: Params) {
-  const post = getPostBySlug(params.slug, [
-    "title",
-    "date",
-    "slug",
-    "author",
-    "content",
-    "ogImage",
-    "coverImage",
-  ]);
-  const content = await markdownToHtml(post.content || "");
-
-  return {
-    props: {
-      post: {
-        ...post,
-        content,
-      },
+// If you need to fetch the post data statically, use getStaticProps
+// This is just an example. Modify according to your data fetching logic.
+export async function getStaticProps() {
+  const content = [
+    {
+      title: `Embark on a Journey of New Beginnings`,
+      excerpt: `As a proud member of the ICCRC (Immigration Consultants of Canada Regulatory Council), Arial Immigration is dedicated to providing genuine, transparent consultation services. We uphold the highest standards of integrity in all our dealings.
+      Guided by our esteemed Managing Director, Mr. Yashpal Jand, a Regulated Canadian Immigration Consultant (RCIC), we ensure our clients are always updated with the latest immigration laws and procedures.`,
+      coverImage: ``,
     },
-  };
-}
+    {
+      title: ``,
+      excerpt: `Our commitment to your journey is reflected in our comprehensive support, from initial profile assessment to offering tailored solutions that enhance your career prospects.
+      At Arial Immigration, customer satisfaction is not just a goal, it's our promise. We address every query with a deep understanding of your emotional and financial considerations.
+      Our consultancy is built on the pillars of honesty, consistency, and strict client confidentiality, making us a trusted partner in your immigration journey.`,
+      coverImage: ``,
+    },
+    {
+      title: ``,
+      excerpt: `As ICCRC-certified consultants, we adhere to rigorous standards set by the Canadian government to regulate immigration, citizenship, and international student advising services.
+      Our role includes enforcing the Code of Professional Ethics, accrediting Immigration Practitioner Programs, administering Full Skills Exams, delivering Practice Management Education, and ensuring continual professional development.
+      Trust Arial Immigration to guide you through every step of your immigration process with expertise and care.`,
+      coverImage: rcic_cric,
+    },
+  ];
 
-export async function getStaticPaths() {
-  const posts = getAllPosts(["slug"]);
+  const post = {
+    title: "About us",
+    coverImage: CoverImage,
+    content: content,
+    slug: "about-us",
+  };
+
+  const headerLinks = [
+    {
+      title: "About Us",
+      slug: "about-us",
+    },
+    {
+      title: "Contact Us",
+      slug: "contact-us",
+    },
+    {
+      title: "Services",
+      slug: "services",
+    },
+    {
+      title: "Insight",
+      slug: "insight",
+    },
+  ];
 
   return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          slug: post.slug,
-        },
-      };
-    }),
-    fallback: false,
+    props: { post, headerLinks },
   };
 }
