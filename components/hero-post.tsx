f65628dfdee6e120c79daa4e96a31c7e927045f7
aryan <aryan.jand@gmail.com>
2024-01-06 17:27:48 -0800

    Remove underline during hover

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -21,9 +21,7 @@ const HeroPost = ({ title, coverImage, date, excerpt, author }: Props) => {
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight hover:underline">
-            {title}
-          </h3>
+          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">{title}</h3>
           <div className="mb-4 md:mb-0 text-lg">
             <DateFormatter dateString={date} />
           </div>
