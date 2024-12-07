4a3f73e51c4013353bf88f36c82c0d1158611a75
aryanjand <aryan.jand@gmail.com>
2024-12-06 22:13:04 -0800

    Added Fav Icon

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,23 +4,20 @@ const Meta = () => {
   return (
     <Head>
       <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="apple-mobile-web-app-title" content="Arial Immgr" />
+      <link rel="manifest" href="/site.webmanifest" />
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
