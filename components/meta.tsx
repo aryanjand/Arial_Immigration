cc23735fb7ed0d60466d79cebdf804098a385c3e
aryanjand <aryan.jand@gmail.com>
2024-12-08 11:19:40 -0800

    updated open graph image

--- components/meta.tsx
+++ components/meta.tsx
@@ -45,7 +45,7 @@ const Meta = () => {
       <meta property="og:type" content="website" />
       <meta
         property="og:image"
-        content="https://www.arialimmigration.ca/assets/images/hero/hero-home-page-open-graph.png"
+        content="https://www.arialimmigration.ca/assets/images/hero/hero-maple-leaf.png"
       />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
@@ -58,7 +58,7 @@ const Meta = () => {
       />
       <meta
         name="twitter:image"
-        content="https://www.arialimmigration.ca/assets/images/hero/hero-home-page-open-graph.png"
+        content="https://www.arialimmigration.ca/assets/images/hero/hero-maple-leaf.png"
       />
     </Head>
   );
