aac26249acca8e6fdd8e3c8619b758817542ff6d
aryanjand <aryan.jand@gmail.com>
2024-12-11 22:52:15 -0800

    Added Hamburger Menu

--- components/app/header.tsx
+++ components/app/header.tsx
@@ -3,46 +3,97 @@ import cn from 'classnames';
 import HeaderLink from '../../interfaces/headerlink';
 import { useRouter } from 'next/router';
 import { COMPANY_NAME } from '../../lib/constants';
+import { useState } from 'react';
 
 type Props = {
   headerLinks: HeaderLink[];
 };
 
 const Header = ({ headerLinks }: Props) => {
-  const router = useRouter();
-  const { pathname } = router;
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const { pathname } = useRouter();
+
+  const title = headerLinks[0];
+  const isCompanyName = title.title === COMPANY_NAME;
+  const isHomePage = title.path === '/home' && pathname === '/home';
+
+  const toggleNav = () => setIsNavOpen((prev) => !prev);
+
+  const renderNavLinks = (isMobile: boolean) => (
+    <ul
+      className={cn({
+        'flex-col items-center min-h-[250px]': isMobile,
+        'hidden lg:flex space-x-20': !isMobile,
+      })}
+    >
+      {headerLinks.slice(1).map((link) => (
+        <li
+          key={link.title}
+          className={cn({
+            'border-b font-bold my-8': isMobile,
+          })}
+        >
+          <Link
+            key={link.path}
+            href={`/${link.path}`}
+            className={cn('hover:underline', {
+              'underline font-bold': link.path === pathname,
+            })}
+          >
+            {link.title.charAt(0).toUpperCase() +
+              link.title.slice(1).toLowerCase()}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <header className="mb-20 mt-8">
       <nav className="flex flex-wrap items-center justify-between">
-        {headerLinks.map((link) => {
-          // Determine if this is the company name link, home page link, or regular nav link
-          const isCompanyName = link.title === COMPANY_NAME;
-          const isHomePage = link.path === '/home' && pathname === '/home';
-          const isActive = link.path === pathname && !isCompanyName;
+        <Link
+          href={`/${title.path}`}
+          className={cn('hover:underline', {
+            'text-4xl md:text-6xl font-bold tracking-tight': isCompanyName,
+            'text-5xl md:text-6xl lg:text-7xl font-bold text-center md:text-left':
+              isHomePage,
+          })}
+        >
+          <h1>{title.title}</h1>
+        </Link>
 
-          return (
-            <nav>
-              <Link
-                key={link.path}
-                href={`/${link.path}`}
-                className={cn('hover:underline', {
-                  'underline font-bold': isActive,
-                  'text-2xl md:text-4xl font-bold tracking-tight':
-                    isCompanyName,
-                  'text-4xl md:text-6xl lg:text-7xl font-bold text-center md:text-left':
-                    isHomePage,
-                })}
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div className="HAMBURGER-ICON space-y-2" onClick={toggleNav}>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <span key={index} className="block h-1 w-8 bg-gray-900"></span>
+            ))}
+          </div>
+
+          {isNavOpen && (
+            <div className="absolute top-0 left-0 w-full h-full bg-white z-10 flex flex-col justify-evenly items-center">
+              <div
+                className="absolute top-0 right-0 px-16 py-8"
+                onClick={toggleNav}
               >
-                {isCompanyName ? (
-                  <h1>{link.title}</h1>
-                ) : (
-                  <span>{link.title}</span>
-                )}
-              </Link>
-            </nav>
-          );
-        })}
+                <svg
+                  className="h-8 w-8 text-gray-900"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18" />
+                  <line x1="6" y1="6" x2="18" y2="18" />
+                </svg>
+              </div>
+              {renderNavLinks(true)}
+            </div>
+          )}
+        </section>
+
+        {renderNavLinks(false)}
       </nav>
     </header>
   );
